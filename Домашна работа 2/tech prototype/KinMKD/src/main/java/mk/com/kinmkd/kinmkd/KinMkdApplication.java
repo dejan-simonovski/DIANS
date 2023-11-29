7020ede33b7a56a8b5168e4fcfe28d266c3dc28a
ivancekikj <ivan.cekic10@gmail.com>
2023-11-29 19:30:19 +0100

    Prototype code init

---
+++ Домашна работа 2/tech prototype/KinMKD/src/main/java/mk/com/kinmkd/kinmkd/KinMkdApplication.java
@@ -0,0 +1,13 @@
+package mk.com.kinmkd.kinmkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinMkdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinMkdApplication.class, args);
+    }
+
+}
